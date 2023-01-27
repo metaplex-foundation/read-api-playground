6cb0005635522460f9383ae9fd3440829d873f2a
jon wong <j@jnwng.com>
2023-01-27 15:16:54 -0500

    Setting the production schema endpoint as the source of truth
    Also added in some example endpoints that we can add to over time!

--- src/examplesList.tsx
+++ src/examplesList.tsx
@@ -1,30 +1,10 @@
 export default [
   {
-    name: "api-with-examples",
-    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/api-with-examples-openrpc.json", //tslint:disable-line
+    name: "Metaplex Reference (devnet)",
+    url: "https://rpc-devnet.aws.metaplex.com/",
   },
   {
-    name: "link-example",
-    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/link-example-openrpc.json", //tslint:disable-line
-  },
-  {
-    name: "params-by-name-petstore",
-    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/params-by-name-petstore-openrpc.json", //tslint:disable-line
-  },
-  {
-    name: "petstore-expanded",
-    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/petstore-expanded-openrpc.json", //tslint:disable-line
-  },
-  {
-    name: "petstore",
-    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/petstore-openrpc.json", //tslint:disable-line
-  },
-  {
-    name: "simple-math",
-    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/simple-math-openrpc.json", //tslint:disable-line
-  },
-  {
-    name: "empty-document",
-    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/empty-openrpc.json", //tslint:disable-line
+    name: "Metaplex Reference (mainnet-beta)",
+    url: "https://read-api.metaplex.com/",
   },
 ];
--- src/fetchUrlSchemaFile.tsx
+++ src/fetchUrlSchemaFile.tsx
@@ -1,7 +1,18 @@
-export default async (schemaUrl: string) => {
-  const response = await fetch(schemaUrl);
+export default async () => {
+  const response = await fetch("https://rpc-devnet.aws.metaplex.com/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      method: "schema",
+      id: "rpd-op-123",
+    }),
+  });
+
   if (response.status === 404) {
     throw new Error("404: Not Found");
   }
-  return response.text();
+
+  const { result } = await response.json();
+  return JSON.stringify(result);
 };
